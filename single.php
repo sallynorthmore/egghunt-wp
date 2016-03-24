dbf05241a6920be3f65dbdadc7178b790ab958b5
Sally Northmore <sally.northmore@gmail.com>
2016-03-24 13:53:19 +0000

    Updates single.php which is used for posts.

--- single.php
+++ single.php
@@ -1,20 +1,31 @@
 <?php get_header(); ?>
-
-<?php if (have_posts()) : while (have_posts()) : the_post();?>
-<section id="main" role="main" class="Post">
-
-	<article id="post_<?php the_ID(); ?>" class="Post-content">
-		<h2 class="Post-title">
-			<?php echo the_title(); ?>
-		</h2>
+<section class="main" id="main">
+	<?php while (have_posts()) : the_post(); ?> 
 	
-		<div class="Post-body">
-			<?php the_content(); ?> 
-		</div>
-	</article>
+	<?php if (has_post_thumbnail( $post->ID )) : ?>
+	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+	<div class="section section--post" style="background-image: url('<?php echo $image['0']; ?>')">
+	<?php else: ?>
+	<div class="section section--post">
+	<?php endif; ?>
+	
+		<div class="section-inner">
+			<!-- <h1 class="section-title"> -->
+				<?php //the_title(); ?>
+			<!-- <h1> -->
 
-    <a href="#">back</a>
+			<?php if ( has_post_thumbnail()) : ?>
+			<?php the_post_thumbnail(); ?>
+			<?php endif; ?>
+
+			<div class="section-body">
+				<?php the_content(); ?>
+			</div>
+
+		</div>
+		
+	</div>
+	<?php endwhile;?>
 
 </section>
-<?php endwhile; ?>
 <?php endif; ?>
